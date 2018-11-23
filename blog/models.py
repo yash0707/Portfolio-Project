7bd7351a1d9576e427279d4006374990bd899291
sherlock07 <yvarshney0707@gmail.com>
2018-11-23 10:38:15 +0530

    Blogs code added

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,13 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    # this is used to display title on the admin page
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
